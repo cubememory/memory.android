e37eb1ed2528b2d92c4017d3ac15ce4280616e2e
wruibo <19396410@qq.com>
2015-06-10 21:12:50 +0800

    add new page

--- src/memory/src/com/cube/memory/ui/ActivityStartPage.java
+++ src/memory/src/com/cube/memory/ui/ActivityStartPage.java
@@ -1,15 +1,32 @@
 package com.cube.memory.ui;
 
+import android.os.Bundle;
+import android.os.Handler;
+
 import com.cube.memory.phone.R;
 
-import android.os.Bundle;
-
 public class ActivityStartPage extends ActivityBase{
-
+	private Handler mHandler;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_start_page);
+		this.gotoHomePageLater();
 	}
-
+	
+	private void gotoHomePageLater(){
+		mHandler = new Handler();
+		mHandler.postDelayed(new Runnable(){
+			@Override
+			public void run() {
+				gotoHomePage();
+			}
+			
+		}, 3000);
+	}
+	
+	private void gotoHomePage(){
+		
+	}
 }
