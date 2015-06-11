ccd4daedabedb06c3ab432990a083898d44b8be1
wruibo <19396410@qq.com>
2015-06-11 15:59:51 +0800

    test git

--- src/memory/src/com/cube/memory/ui/ActivityStartPage.java
+++ src/memory/src/com/cube/memory/ui/ActivityStartPage.java
@@ -31,5 +31,6 @@ public class ActivityStartPage extends ActivityBase{
 		Intent intent = new Intent(this, ActivityHomePage.class);
 		this.startActivity(intent);
 		this.finish();
+		//for test git
 	}
 }
